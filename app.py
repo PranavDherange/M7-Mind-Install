6a1c70a9374b87e20c50de22c427edf883ca3b26
krshn-ptl <58908961+krshn-ptl@users.noreply.github.com>
2021-02-06 20:50:40 +0530

    kcac

--- app.py
+++ app.py
@@ -1,23 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask 
 
+app = Flask(__name__) 
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
-
-@app.route("/voiceToText", methods=["POST"])
-def voiceToText():
-    # Loading speech recognition model and feeding user audio
-    # Returning the response from model
-    return "Voice"
-    
-@app.route("/predictEmotion", methods=["POST"])
-def predictEmotion():
-    # Loading trained model and feeding it text
-    # Returning the emotion 
-    return "Emotion"
-
-if __name__ == "__main__":
-    print("Starting Python Flask Server")
-    app.run(debug=True
+@app.route("/") 
+def home_view(): 
+		return "<h1>Welcome to Geeks for Geeks</h1>"
