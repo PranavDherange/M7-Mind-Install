954c67a930392a23a73d96dd7a4a517cea084d8c
krshn-ptl <58908961+krshn-ptl@users.noreply.github.com>
2021-02-06 23:03:28 +0530

    flutter addition

--- insomniac/README.md
+++ insomniac/README.md
@@ -1,4 +1,4 @@
-# zominac
+# insomniac
 
 A new Flutter project.
 
--- insomniac/android/app/src/main/kotlin/com/example/insomniac/MainActivity.kt
+++ insomniac/android/app/src/main/kotlin/com/example/insomniac/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.zominac
+package com.example.insomniac
 
 import io.flutter.embedding.android.FlutterActivity
 
