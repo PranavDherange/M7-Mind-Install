75c9547d7b60f8f98081c14f769c6836d12089cf
Astel <astelthottankara@gmail.com>
2021-02-06 10:17:27 +0530

    data: add more posts

--- reddit-data/app.py
+++ reddit-data/app.py
@@ -3,20 +3,23 @@ import pandas as pd
 
 posts = []
 
+# Creating instance praw
 reddit = praw.Reddit(client_id='e6qeyZCLeP9IQQ', client_secret='l_6dsZYdeYXvvLuO4MZ1v2Sx-5w4Lw', user_agent='Reddit WebScraping')
 
 # get all posts from the subreddit
-sw_subreddit = reddit.subreddit('SuicideWatch').hot(limit=10)
+sw_subreddit = reddit.subreddit('SuicideWatch').hot(limit=40)
+
 for post in sw_subreddit:
     # Adding main thread details
     posts.append([post.title])
     
-    # Handling replies
+    # Adding replies of current thread
     submission = reddit.submission(url=post.url)
     submission.comments.replace_more(limit=0)
     for comment in submission.comments.list():
         posts.append([comment.body])
 
+# Conversion and exporting
 posts = pd.DataFrame(posts,columns=['body'])
 print(posts)
 
