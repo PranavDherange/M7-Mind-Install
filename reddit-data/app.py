a1da7d5a9cd6174d5ef7e4c088106de6ac935015
Astel <astelthottankara@gmail.com>
2021-02-06 11:19:48 +0530

    data: add other reddit's data

--- reddit-data/app.py
+++ reddit-data/app.py
@@ -8,8 +8,19 @@ reddit = praw.Reddit(client_id='e6qeyZCLeP9IQQ', client_secret='l_6dsZYdeYXvvLuO
 
 # get all posts from the subreddit
 sw_subreddit = reddit.subreddit('SuicideWatch').hot(limit=40)
+ab_subreddit = reddit.subreddit('abuse').hot(limit=20)
 
 for post in sw_subreddit:
+    # Adding main thread details
+    posts.append([post.title])
+    
+    # Adding replies of current thread
+    submission = reddit.submission(url=post.url)
+    submission.comments.replace_more(limit=0)
+    for comment in submission.comments.list():
+        posts.append([comment.body])
+
+for post in ab_subreddit:
     # Adding main thread details
     posts.append([post.title])
     
