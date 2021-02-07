8d76c3b154f9843a854065bcbbb7367f6d56bc01
krshn-ptl <58908961+krshn-ptl@users.noreply.github.com>
2021-02-07 16:35:42 +0530

    Flutter Stage 1

--- insomniac/lib/main.dart
+++ insomniac/lib/main.dart
@@ -1,106 +1,101 @@
-import 'dart:convert';
 import 'dart:html';
-import 'package:http/http.dart' as http;
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+// import 'package:speech_to_text/speech_to_text.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:http/http.dart' as http;
+
+// import 'package:speech_to_text/speech_to_text_web.dart';
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MyHomePage extends StatelessWidget {
+  void switchScreen(str, context) => Navigator.push(
+      context, MaterialPageRoute(builder: (context) => UploadPage(url: str)));
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    TextEditingController controller = TextEditingController();
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+              "Insert the URL that will receive the Multipart POST request (including the starting http://)",
+              style: Theme.of(context).textTheme.headline),
+          TextField(
+            controller: controller,
+            onSubmitted: (str) => switchScreen(str, context),
+          ),
+          FlatButton(
+            child: Text("Take me to the upload screen"),
+            onPressed: () => switchScreen(controller.text, context),
+          )
+        ],
+      ),
+    ));
+  }
 }
 
-const hostURL = "http://127.0.0.1:5000/hi";
-// const hostURL = "https://zomniac.herokuapp.com/";
+class UploadPage extends StatefulWidget {
+  UploadPage({Key key, this.url}) : super(key: key);
 
-class _MyHomePageState extends State<MyHomePage> {
-  // Future<void> _listenbackend() async {
-  //   bool _service;
-  //   _service = await _getValue();
-  // }
+  final String url;
+
+  @override
+  _UploadPageState createState() => _UploadPageState();
+}
+class _UploadPageState extends State<UploadPage> {
+
+  Future<String> uploadAudio(filename, url) async {
+    var request = http.MultipartRequest('POST', Uri.parse(url));
+    request.files.add(await http.MultipartFile.fromPath('audio', filename));
+    var res = await request.send();
+    return res.reasonPhrase;
+  }
+  String state = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 20,
-        centerTitle: true,
-        title: Text("Demo"),
+        title: Text('Flutter File Upload Example'),
       ),
-      body: Container(
-        child: _getValue(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(state)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          FilePickerResult result =await FilePicker.platform.pickFiles(type: FileType.audio);
+          if (result != null) {
+            var res = await uploadAudio(result.files.first.path, widget.url);
+          setState(() {
+            state = res;
+            print(res);
+          });
+            }
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
-}
-
-http.Client _client() {
-  return http.Client();
-}
-
-// void check(){
-//   if(_getValue()==true){
-//     _addText(str)
-//   }
-// }
-
-Widget _getValue() {
-  var client = _client();
-  try {
-    client.post(
-      hostURL,
-      body: {"Query": "bla"},
-    )..then((res) {
-        Map<String, dynamic> data = jsonDecode(res.body);
-        _addText(data['response']);
-      });
-    // return true;
-  } catch (e) {
-    print(e);
-    // return false;
-  } finally {
-    client.close();
-  }
-}
-
-void _addText(String str) {
-  Text(str);
 }
